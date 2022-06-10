d2dbfdc96f64a58f296a2f1d39c0d40a11fa2d67
Shuo Wang <shuooo@gmail.com>
2022-06-09 21:36:17 -0400

    c

---
+++ 01/134.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if (sum(gas) - sum(cost) < 0):
+            return -1
+        tank, start = 0, 0
+        for i in range(len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                start = i+1
+                tank = 0
+        return start
