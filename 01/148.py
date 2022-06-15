14e7a31011b05c685a40ac62a93a5514533fc6a6
Shuo Wang <shuooo@gmail.com>
2022-06-14 22:18:25 -0400

    c

---
+++ 01/148.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def merge(l, r):
+            if not l or not r:
+                return l or r
+            dummy = p = ListNode(0)
+            while l and r:
+                if l.val < r.val:
+                    p.next = l
+                    l = l.next
+                else:
+                    p.next = r
+                    r = r.next
+                p = p.next
+            p.next = l or r
+            return dummy.next
+        
+        if not head or not head.next:
+            return head
+        fast, slow = head.next, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        start = slow.next
+        slow.next = None
+        l, r = self.sortList(head), self.sortList(start)
+        return merge(l, r)
